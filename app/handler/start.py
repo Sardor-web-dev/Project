b156e472a52a14cb38088eda9c37d26c12f23415
gomm5324-tech <gomm5324@gmail.com>
2026-06-08 19:15:46 +0500

    fixed command start

--- app/handler/start.py
+++ app/handler/start.py
@@ -7,4 +7,4 @@ router = Router()
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer("Это команда Старт")
+    await message.answer("Это онлайн интернет магазин! Что желаете купить?")
