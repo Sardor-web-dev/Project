024c653434c046d8f1bdb4984e1f1879f736d0c5
Sardor-web-dev <dzamolovsardor5@gmail.com>
2026-06-10 18:40:52 +0500

    implemented menu

---
+++ app/handler/menu.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from app.keyboards.reply import menu_keyboard
+
+router = Router()
+
+
+@router.message(Command("menu"))
+async def command_menu_handler(message: Message) -> None:
+    await message.answer("Выберите раздел:", reply_markup=menu_keyboard())
