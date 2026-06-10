c9e1b55b7eaa22ee4519c1b434e34bec4cae54e2
Sardor-web-dev <dzamolovsardor5@gmail.com>
2026-06-10 19:00:09 +0500

    implemented discount card

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from aiogram import Bot, Dispatcher
 from app.handler.start import router as start_router
 from app.handler.command_help import router as help_router
 from app.handler.menu import router as menu_router
+from app.handler.card import router as card_router
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN") or ""
@@ -18,7 +19,7 @@ setup_logger()
 logger = logging.getLogger(__name__)
 
 dp = Dispatcher()
-dp.include_routers(start_router, help_router, menu_router)
+dp.include_routers(start_router, help_router, menu_router, card_router)
 
 async def main():
     bot = Bot(token=TOKEN)
