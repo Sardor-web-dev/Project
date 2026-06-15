67b02ab3c84eac62aabd08fb647035d563ee4b8b
gomm5324-tech <gomm5324@gmail.com>
2026-06-15 18:43:58 +0500

    implemented products and categories functionality

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from app.handler.start import router as start_router
 from app.handler.command_help import router as help_router
 from app.handler.menu import router as menu_router
 from app.handler.card import router as card_router
+from app.handler.products import router as products_router
+
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN") or ""
@@ -19,9 +21,9 @@ setup_logger()
 logger = logging.getLogger(__name__)
 
 dp = Dispatcher()
-dp.include_routers(start_router, help_router, menu_router, card_router)
+dp.include_routers(start_router, help_router, menu_router, card_router, products_router)
 
-async def main():
+async def main():j
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
